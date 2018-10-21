08a012901e1d1f2ccf0286f2bdcc65f60b5b2174
Fernando Palazuelo <DatFerPa@users.noreply.github.com>
2018-10-21 20:18:02 +0200

    añadiendo fotos de manera cutre

--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/MenuPrincipalActivity.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/MenuPrincipalActivity.java
@@ -2,12 +2,32 @@ package com.filmaffinityparajuegos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    ArrayList<ImageButton> botones = new ArrayList<ImageButton>();
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        layout = findViewById(R.id.LayoutMain);
+
+        generateBotones();
+
+
+    }
+    private void generateBotones() {
+        for(int i = 0; i <10; i++){
+            ImageButton buttonI;
+            buttonI = new ImageButton(getApplicationContext());
+            buttonI.setImageResource((i%2 ==0)?R.drawable.godofwarmini :R.drawable.legendofkaymini);
+            layout.addView(buttonI);
+        }
     }
 }
