0080bb5a15331cf405d806985ef7c29e7ef8f7f4
Fernando Palazuelo <fpalazuelo@gmail.com>
2018-12-08 19:17:21 +0100

    cosas con la api

--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/MenuPrincipalActivity.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/MenuPrincipalActivity.java
@@ -1,6 +1,7 @@
 package com.filmaffinityparajuegos;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,17 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.android.volley.VolleyError;
+import com.filmaffinityparajuegos.data.Videojuego;
+import com.igdb.api_android_java.callback.OnSuccessCallback;
 import com.igdb.api_android_java.wrapper.IGDBWrapper;
+import com.igdb.api_android_java.wrapper.Parameters;
 import com.igdb.api_android_java.wrapper.Version;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -17,6 +27,8 @@ public class MenuPrincipalActivity extends AppCompatActivity {
 
     ArrayList<ImageButton> botones = new ArrayList<ImageButton>();
     LinearLayout layout;
+    ArrayList<Videojuego> videojuegosNuevos = new ArrayList<>();
+    ArrayList<Videojuego> videojuegosPopulares = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +36,43 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu_principal);
         layout = findViewById(R.id.LayoutMain);
         IGDBWrapper wrapper = new IGDBWrapper(getApplicationContext() , "cec1dc5cac50616ebc4643c7bc94647c", Version.STANDARD,false);
+        Parameters params = new Parameters().addFields("*").addFilter("[category][eq]=0").addOrder("published_at:desc").addLimit("6");
+        wrapper.games(params, new OnSuccessCallback() {
+            @Override
+            public void onSuccess(@NotNull JSONArray jsonArray) {
+                System.out.println(jsonArray.toString());
+
+                for(int i = 0; i < jsonArray.length();i++){
+                    try {
+                        JSONObject obj = jsonArray.getJSONObject(i);
+                        Videojuego juego = new Videojuego();
+                        juego.setId_juego(obj.getString("id"));
+                        juego.setDescripcion(obj.getString("summary"));
+                        juego.setTitulo(obj.getString("name"));
+                       // juego.setId_developer(obj.getString(""));
+                        juego.setUri_imagen(obj.getJSONObject("cover").getString("url"));
+                        videojuegosNuevos.add(juego);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onError(@NotNull VolleyError volleyError) {
+
+            }
+        });
         generateBotones();
-
-
     }
     private void generateBotones() {
 
-        for(int i = 0; i <10; i++){
+        for(int i = 0; i <videojuegosNuevos.size(); i++){
             ImageButton buttonI;
             buttonI = new ImageButton(getApplicationContext());
-            buttonI.setImageResource((i%2 ==0)?R.drawable.godofwarmini :R.drawable.legendofkaymini);
+            buttonI.setImageURI( Uri.parse(videojuegosNuevos.get(i).getUri_imagen()));
             buttonI.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -42,7 +81,6 @@ public class MenuPrincipalActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Ha clicado en un juego", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(getApplicationContext(), DetallesActivity.class);
                     startActivity(intent);
-
                 }
             });
             layout.addView(buttonI);
--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/data/Videojuego.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/data/Videojuego.java
@@ -12,7 +12,51 @@ public class Videojuego {
     public String id_developer;
 
 
+    public String getId_juego() {
+        return id_juego;
+    }
 
+    public void setId_juego(String id_juego) {
+        this.id_juego = id_juego;
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUri_imagen() {
+        return uri_imagen;
+    }
+
+    public void setUri_imagen(String uri_imagen) {
+        this.uri_imagen = uri_imagen;
+    }
+
+    public String getUri_video() {
+        return uri_video;
+    }
+
+    public void setUri_video(String uri_video) {
+        this.uri_video = uri_video;
+    }
+
+    public String getId_developer() {
+        return id_developer;
+    }
+
+    public void setId_developer(String id_developer) {
+        this.id_developer = id_developer;
+    }
 }
