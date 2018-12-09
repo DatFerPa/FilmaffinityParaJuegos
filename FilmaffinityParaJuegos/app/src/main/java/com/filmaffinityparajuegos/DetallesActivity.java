e2020db8a57828ff6332aacdf360d4a700a6f97c
Juan Granda Molaguero <uo244759@uniovi.es>
2018-12-09 19:30:26 +0100

    Tope guapo la vista ahi

--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/DetallesActivity.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/DetallesActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.filmaffinityparajuegos.data.Videojuego;
 import com.squareup.picasso.Picasso;
@@ -13,7 +14,8 @@ public class DetallesActivity extends AppCompatActivity {
 
     Videojuego videojuego;
     ImageView imagenVideojuego;
-
+    TextView tituloVideojuego;
+    TextView descripcionVideojuego;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +23,11 @@ public class DetallesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalles);
         Intent intent = getIntent();
         videojuego = intent.getParcelableExtra(MenuPrincipalActivity.NV);
-        System.out.println(videojuego.getDescripcion());
         imagenVideojuego = (ImageView) findViewById(R.id.imagenJuego);
+        tituloVideojuego = (TextView) findViewById(R.id.id_titulo);
+        descripcionVideojuego = (TextView) findViewById(R.id.id_resumen);
+        tituloVideojuego.setText(videojuego.getTitulo());
+        descripcionVideojuego.setText(videojuego.getDescripcion());
         Picasso.get().load(Uri.parse(videojuego.getUri_imagen())).resize(500  ,500).into(imagenVideojuego);
 
     }
