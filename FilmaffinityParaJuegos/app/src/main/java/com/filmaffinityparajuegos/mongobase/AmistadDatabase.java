9f87723ad2ec4b036d23321d205fd946210916c1
Juan Granda Molaguero <uo244759@uniovi.es>
2019-01-07 19:29:02 +0100

    Base de datos
    a ver si funchiona esto

--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/AmistadDatabase.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/AmistadDatabase.java
@@ -1,19 +1,59 @@
 package com.filmaffinityparajuegos.mongobase;
 
+import com.filmaffinityparajuegos.data.Amistad;
 import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+
 
 public class AmistadDatabase {
 
-    DB base = new MongoConnection().getClient().getDB("videojogos");
+    MongoDatabase base = new MongoConnection().getClient().getDatabase("videojogos");
 
     public void addAmistad(String nombre1, String nombre2){
-        DBCollection usuarios =  base.getCollection("amistades");
-        BasicDBObject usuario = new BasicDBObject();
-        usuario.put("usuario1",nombre1);
-        usuario.put("usuario2", nombre2);
+        MongoCollection<Document> amistades =  base.getCollection("amistades");
 
-        usuarios.insert(usuario);
+        Document amistad = new Document("usuario1", nombre1).append("usuario2" , nombre2);
+
+
+        amistades.insertOne(amistad);
     }
+
+    public List<Amistad> getAmistades(String usuario1){
+        final List<Amistad> amistades = new ArrayList<Amistad>();
+
+        MongoCollection<Document> collection = base.getCollection("amistades");
+
+        Block<Document> amistades_base = new Block<Document>() {
+            @Override
+            public void apply(Document document) {
+                String usuario1 = document.get("usuario1").toString();
+                String usuario2 = document.get("usuario2").toString();
+
+                Amistad amistad = new Amistad(usuario1,usuario2);
+
+                amistades.add(amistad);
+            }
+        };
+
+        collection.find(eq("usuario1",usuario1)).forEach(amistades_base);
+
+
+
+
+        return amistades;
+    }
+
+
 }
--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/UserDatabase.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/UserDatabase.java
@@ -4,22 +4,23 @@ import com.filmaffinityparajuegos.data.Usuario;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import org.bson.Document;
+
 public class UserDatabase {
-    DB base = new MongoConnection().getClient().getDB("videojogos");
+    MongoDatabase base = new MongoConnection().getClient().getDatabase("videojogos");
 
     public UserDatabase() {
     }
 
     public void addUser(String name, String password){
 
-        DBCollection usuarios =  base.getCollection("usuarios");
-        BasicDBObject usuario = new BasicDBObject();
-        usuario.put("name",name);
-        usuario.put("password", password);
+        MongoCollection<Document> usuarios =  base.getCollection("usuarios");
+        Document usuario = new Document("name", name).append("password" , password);
 
-        usuarios.insert(usuario);
+        usuarios.insertOne(usuario);
     }
 
 }
