4874e552073c20e1a04043bd775326a3abe3e79c
Juan Granda Molaguero <uo244759@uniovi.es>
2019-01-08 18:54:51 +0100

    Base de datos actualizada

--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/AmistadesDatabase.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/AmistadesDatabase.java
@@ -2,56 +2,43 @@ package com.filmaffinityparajuegos.mongobase;
 
 import android.content.Context;
 import android.util.Log;
-import android.view.View;
 
-import com.android.volley.Request.Method;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.RequestFuture;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.filmaffinityparajuegos.data.Amistad;
 import com.filmaffinityparajuegos.data.Usuario;
-import com.mongodb.util.JSON;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.runner.Request;
 
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import okhttp3.OkHttpClient;
+public class AmistadesDatabase {
 
-public class UsuarioDatabase {
-
-    String base = "https://api.mlab.com/api/1/databases/videojogos/collections/usuarios";
+    String base = "https://api.mlab.com/api/1/databases/videojogos/collections/amistades";
     String claveConsulta = "&apiKey=CfF8SL07e7ZNkcOA1l0yWQ_j3VSKJBpZ";
     String clave = "?apiKey=CfF8SL07e7ZNkcOA1l0yWQ_j3VSKJBpZ";
-    Usuario usuario;
-
-
     private String TAG = "SO_TEST";
+    List<Amistad> amistades;
 
-    public JSONArray getUser(String name, String password, Context context) {
-        String consulta = base + "?q={\"name\":\"" + name + "\"}" + claveConsulta;
+    public JSONArray getAmistades(String usuario1, Context context){
+        String consulta = base + "?q={\"seguidor\":\"" + usuario1 + "\"}" + claveConsulta;
         RequestQueue request = Volley.newRequestQueue(context);
         RequestFuture<JSONArray> future = RequestFuture.newFuture();
         JSONArray response = null;
         System.out.println(consulta);
 
-        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Method.GET, consulta, null, future, future);
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, consulta, null, future, future);
 
         request.add(jsonObjectRequest);
 
@@ -70,16 +57,16 @@ public class UsuarioDatabase {
         return response;
     }
 
-    public JSONArray addUser(String name, String password, Context context) {
+    public JSONArray addAmistad(String seguidor, String seguido, Context context) {
 
         String consulta = base + clave;
         RequestQueue request = Volley.newRequestQueue(context);
         RequestFuture<JSONObject> future = RequestFuture.newFuture();
         JSONArray response = new JSONArray();
-        Map<String, String> usuario = new HashMap<String, String>();
-        usuario.put("name", name);
-        usuario.put("password", password);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Method.POST, consulta, new JSONObject(usuario), future, future);
+        Map<String, String> amistad = new HashMap<String, String>();
+        amistad.put("seguidor", seguidor);
+        amistad.put("seguido", seguido);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, consulta, new JSONObject(amistad), future, future);
         request.add(jsonObjectRequest);
         try {
             response.put(0, future.get(3, TimeUnit.SECONDS));
--- FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/VideojuegosDatabase.java
+++ FilmaffinityParaJuegos/app/src/main/java/com/filmaffinityparajuegos/mongobase/VideojuegosDatabase.java
@@ -1,4 +1,87 @@
 package com.filmaffinityparajuegos.mongobase;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.Volley;
+import com.filmaffinityparajuegos.data.Usuario;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 public class VideojuegosDatabase {
+    String base = "https://api.mlab.com/api/1/databases/videojogos/collections/videojuegos";
+    String claveConsulta = "&apiKey=CfF8SL07e7ZNkcOA1l0yWQ_j3VSKJBpZ";
+    String clave = "?apiKey=CfF8SL07e7ZNkcOA1l0yWQ_j3VSKJBpZ";
+    Usuario usuario;
+
+
+    private String TAG = "SO_TEST";
+
+    public JSONArray getVideojuego(String name, Context context) {
+        String consulta = base + "?q={\"id_videojuego\":\"" + name + "\"}" + claveConsulta;
+        RequestQueue request = Volley.newRequestQueue(context);
+        RequestFuture<JSONArray> future = RequestFuture.newFuture();
+        JSONArray response = null;
+        System.out.println(consulta);
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, consulta, null, future, future);
+
+        request.add(jsonObjectRequest);
+
+        try {
+            response = future.get(3, TimeUnit.SECONDS); // Blocks for at most 10 seconds.
+        } catch (InterruptedException e) {
+            Log.d(TAG, "interrupted");
+        } catch (ExecutionException e) {
+            Log.d(TAG, "execution");
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, response.toString());
+
+        return response;
+    }
+
+    public JSONArray addVideojuego(String id_videojuego,String usuario, String comentario,Double valoracion, Context context) {
+
+        String consulta = base + clave;
+        RequestQueue request = Volley.newRequestQueue(context);
+        RequestFuture<JSONObject> future = RequestFuture.newFuture();
+        JSONArray response = new JSONArray();
+        Map<String, String> videojuego = new HashMap<String, String>();
+        videojuego.put("id_videojuego", id_videojuego);
+        videojuego.put("usuario", usuario);
+        videojuego.put("comentario", comentario);
+        videojuego.put("valoracion", valoracion.toString());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, consulta, new JSONObject(videojuego), future, future);
+        request.add(jsonObjectRequest);
+        try {
+            response.put(0, future.get(3, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            Log.d(TAG, "interrupted");
+        } catch (ExecutionException e) {
+            Log.d(TAG, "execution");
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+
+    }
 }
